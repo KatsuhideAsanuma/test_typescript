27b2763c3e7ed3a18bbadeb12c7a14708f0e206c
KatsuhideAsanuma <katsuhide.asanuma@gmail.com>
2023-01-25 04:10:05 +0900

    main

--- app.ts
+++ app.ts
@@ -1,1 +1,40 @@
 console.log("test typescript");
+const nums:number[]=[10,20,30];
+const strs:string[]=["alpha","beta"];
+const num2:number[]=[...nums];
+num2.push(40);
+console.log(num2,nums);
+let tup:[string,number,number];
+tup=["tt",0,2];
+enum Animal{
+    Cat=100,
+    Dog,
+    Elephant,
+    Tiger
+}
+let g:Animal=Animal.Tiger
+console.log(Animal[g])
+const tools:{[index:number]:string}={};
+tools[0]="mem";
+tools[1000]="thon";
+tools[501]="bag";
+delete tools[1000]
+console.log(tools)
+let sn:string|boolean=true;
+sn="binbin";
+let sns:(string|number)[]=["dik",0,0,"ff"];
+console.log(sns)
+let d:unknown='100';
+let len=(d as string).length
+function unknownTest(i:unknown){
+    if (typeof i==="string"){
+        console.log(i.toLowerCase());
+    }else if(typeof i==="number"){
+        console.log(i+10);
+    }else if(typeof i==="boolean"){
+        console.log(!i)
+    }
+}
+unknownTest("milk")
+unknownTest(20)
+unknownTest(true)
